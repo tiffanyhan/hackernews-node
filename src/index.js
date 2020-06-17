1df9450286a37cf4c2c93d2d6b5fc4ca47467a7c
Tiffany Han <tiffanyhan@tiffanys-mbp.lan>
2020-06-16 18:36:52 -0700

    added user authentication

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: (root, args, context, info) => {
-            return context.prisma.links()
-        },
-        // link: (parent, args) => links.find(link => link.id === args.id)
-    },
-    Mutation: {
-        post: (root, args, context) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description,
-            })
-        },
+    Query,
+    Mutation,
+    User,
+    Link,
         // updateLink: (parent, args) => {
         //     link = links.find(link => link.id === args.id)
         //     if (!link) throw Error('Could not find link with this id')
@@ -30,13 +25,17 @@ const resolvers = {
         //     links.splice(index, 1)
         //     return link
         // },
-    },
 }
 
 // 3
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: { prisma },
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
+    }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
