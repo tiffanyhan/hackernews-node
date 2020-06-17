65ae289cddfec7c17d91bba859d949f744fae55d
Tiffany Han <tiffanyhan@tiffanys-mbp.lan>
2020-06-16 17:39:10 -0700

    added prisma

--- src/index.js
+++ src/index.js
@@ -1,42 +1,35 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-let idCount = links.length
+const { prisma } = require('./generated/prisma-client')
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-        link: (parent, args) => links.find(link => link.id === args.id)
+        feed: (root, args, context, info) => {
+            return context.prisma.links()
+        },
+        // link: (parent, args) => links.find(link => link.id === args.id)
     },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
+        post: (root, args, context) => {
+            return context.prisma.createLink({
                 url: args.url,
-            }
-            links.push(link)
-            return link
+                description: args.description,
+            })
         },
-        updateLink: (parent, args) => {
-            link = links.find(link => link.id === args.id)
-            if (!link) throw Error('Could not find link with this id')
-            link.url = args.url || link.url
-            link.description = args.description || link.description
-            return link
-        },
-        deleteLink: (parent, args) => {
-            index = links.findIndex(link => link.id === args.id)
-            if (index === -1) throw Error('Could not find link with this id')
-            link = links[index]
-            links.splice(index, 1)
-            return link
-        },
+        // updateLink: (parent, args) => {
+        //     link = links.find(link => link.id === args.id)
+        //     if (!link) throw Error('Could not find link with this id')
+        //     link.url = args.url || link.url
+        //     link.description = args.description || link.description
+        //     return link
+        // },
+        // deleteLink: (parent, args) => {
+        //     index = links.findIndex(link => link.id === args.id)
+        //     if (index === -1) throw Error('Could not find link with this id')
+        //     link = links[index]
+        //     links.splice(index, 1)
+        //     return link
+        // },
     },
 }
 
@@ -44,5 +37,6 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: { prisma },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
